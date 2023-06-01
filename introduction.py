99262643fbcc3cf5b376141f3abe90078e74eb54
UKisaanth <134379921+UKisaanth@users.noreply.github.com>
2023-06-01 08:55:32 +0530

    project 1 my edition

---
+++ introduction.py
@@ -0,0 +1,9 @@
+print ('hello, world')
+person=input('what is your name')
+print ('hello',person)
+
+P1=input('What is your project?')
+print ('Ohh, You are doing',P1)
+print ('Good luck to be in 100')
+A1=input('How many projects have you done')
+print (A1,'projects.I think it is easy.anyway goodluck bye')
